2ec1499afcd15e9bf02247839da9e86bddd19dd4
Guneet Aggarwal <guneetaggarwal@gmail.com>
2025-12-12 17:05:14 +0530

    nvim: TroubleBuffer

--- nvim/.config/nvim/lua/custom/plugins/trouble.lua
+++ nvim/.config/nvim/lua/custom/plugins/trouble.lua
@@ -1,5 +1,11 @@
 return {
   'folke/trouble.nvim',
-  opts = {},
+  lazy = false,
   cmd = 'Trouble',
+  config = function()
+    vim.api.nvim_create_user_command('TroubleBuffer', function()
+      vim.cmd 'Trouble diagnostics toggle filter = {buf = 0}'
+    end, { desc = 'Open Trouble for current buffer' })
+    require('trouble').setup {}
+  end,
 }
